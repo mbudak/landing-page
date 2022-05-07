550d6263b95d21f5ed7fb2cf74ddeac6eb722128
mbudak <promaxcoq@gmail.com>
2022-05-07 08:44:58 -0400

    masonry init

---
+++ index.js
@@ -0,0 +1,8 @@
+const wave = String.fromCodePoint(0x1F44B);
+const flame = String.fromCodePoint(0x1F525);
+
+console.log('%c' + wave + wave + ' Hi There ' + wave + wave, 'font-size: 2em;color: #fff;background:#0077b6');
+
+console.log('%c Looks like you know something about how to code!' + flame , 'font-size: 1.2em;color: #fff;background:#0077b6');
+console.log('%c It is really fun to work with us', 'font-size: 1em;color: #fff;background:#0077b6');
+console.log('%c Reach us and let"s talk about what can we do together', 'font-size: 1.2em;color: #e63946;background:#f1faee');
